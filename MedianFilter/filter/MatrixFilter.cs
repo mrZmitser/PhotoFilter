dd64b9ae89506e2c262f333c6eba48baa40f7dcf
Konstantin <zkonstr@gmail.com>
2021-07-01 19:05:33 +0300

    Feat: median filter for matrix has been created

---
+++ MedianFilter/filter/MatrixFilter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace ConsoleFilter.filter
+{
+    public class MatrixFilter
+    {
+        public MatrixFilter(int[,] matrix)
+        {
+            Matrix = matrix;
+        }
+
+        public int[,] Matrix { get; set; }
+
+        public int[,] Filter(int radius)
+        {
+            int[,] filtered = new int[Matrix.GetLength(0), Matrix.GetLength(1)];
+            // TODO : implement me :-)
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    filtered[i, j] = DoFilter(i, j, radius);
+                }
+            }
+
+            return filtered;
+        }
+
+        private int DoFilter(int x, int y, int radius)
+        {
+            var neighbors = new List<int>((radius + 2) * (radius + 2));
+            for (int i = x - radius; i <= x + radius; i++)
+            {
+                for (int j = y - radius; j <= y + radius; j++)
+                {
+                    if (i < 0 || j < 0 || i >= Matrix.GetLength(0) || j >= Matrix.GetLength(1))
+                    {
+                        continue;
+                    }
+
+                    neighbors.Add(Matrix[i, j]);
+                }
+            }
+
+            neighbors.Sort();
+            if (neighbors.Count % 2 == 0)
+            {
+                return neighbors[((neighbors.Count / 2 - 1) + (neighbors.Count / 2)) / 2];
+            }
+
+            return neighbors[(neighbors.Count / 2)];
+        }
+    }
+}
---
+++ MedianFilter/main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ConsoleFilter.filter;
+
+namespace ConsoleFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3,3]{{1,2,3},{4,6,5},{7,8,9}};
+            int r = 1;
+            MatrixFilter Oleg = new MatrixFilter(a);
+            var b =Oleg.Filter(r);
+            foreach (var item in b)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
